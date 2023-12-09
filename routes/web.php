eed9b027282e9614e5304cb54c138ea2bdf8e04b
jembaray <raymondndjie@icloud.com>
2023-12-09 10:19:10 +0100

    u

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about',function(){
+    return view('about');
+});
+
+Route::get("/Login",function(){
+    return view('Login');
+});
+
+
+Route::get("/Footer",function(){
+    return view('');
+});
